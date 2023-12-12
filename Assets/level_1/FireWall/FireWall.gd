10c919c35e271bd9b95665d91960b5d449ffc8ae
S.A. Reemeijer <s.a.reemeijer@umail.leidenuniv.nl>
2023-12-12 14:11:45 +0100

    Kleine fixes

--- Assets/level_1/FireWall/FireWall.gd
+++ Assets/level_1/FireWall/FireWall.gd
@@ -3,6 +3,6 @@ extends Area2D
  
 func _on_body_entered(body: Node2D) -> void:
 	if body.has_method("die"):
-		if PlayerItems.HasFireShield == 0:
+		if PlayerItems.HasFireShield == false:
 			body.die()
  
