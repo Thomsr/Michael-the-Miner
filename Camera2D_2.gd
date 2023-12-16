d03e240f36bcea3f33b6a6480e4c77126ff1a1aa
villathalia <125035822+villathalia@users.noreply.github.com>
2023-12-16 22:40:20 +0100

    Add files via upload
    most recent files

---
+++ Camera2D_2.gd
@@ -0,0 +1,20 @@
+extends Camera2D
+
+var entered = false
+
+func _on_area_2d_body_entered(body):
+	entered = true
+
+
+func _on_area_2d_body_exited(body):
+	entered = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if entered == true:
+		global_position.x = 900.0
+		global_position.y = 500.0
+	else:
+		global_position.x = 2875.0
+		global_position.y = 600.0
+
---
+++ levelchange2.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.has_method("die"):
+		get_tree().change_scene_to_file("res://Levels/Level3/level3.tscn")
+ 
