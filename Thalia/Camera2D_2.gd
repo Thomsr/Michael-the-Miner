b76570f559d8e2ed2aa199e65ac8db63e140b0d9
Thomas Rooskens <s3339661@vuw.leidenuniv.nl>
2023-12-22 14:23:09 +0100

    level3 fix

--- Thalia/Camera2D_2.gd
+++ Thalia/Camera2D_2.gd
@@ -1,23 +1,7 @@
 extends Camera2D
 
-var entered = false
-
-
-
 func _on_firstpart_body_entered(body):
-	entered = true
-
+	global_position.x = 0
 
 func _on_firstpart_body_exited(body):
-	entered = false
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if entered == true:
-		global_position.x = 900.0
-		global_position.y = 500.0
-	else:
-		global_position.x = 2875.0
-		global_position.y = 600.0
-
-
+	global_position.x = 1920
--- Thalia/spikes.gd
+++ Thalia/spikes.gd
@@ -5,6 +5,6 @@ func _on_out_of_the_world_body_entered(body):
 	if body.has_method("die"):
 		body.die()
 
-func _on_spike_area_body_entered(body):
+func _on_body_entered(body):
 	if body.has_method("die"):
 		body.die()
